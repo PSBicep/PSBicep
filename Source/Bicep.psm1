346bd91e2a4d5d312fbc24844d1ef4f8a77f8090
Stefan Ivemo <stefan@ivemo.se>
2021-01-12 21:02:23 +0100

    Updated module

--- Source/Bicep.psm1
+++ Source/Bicep.psm1
@@ -16,27 +16,57 @@ function Invoke-BicepBuild {
         } 
     }
     else {
-        Write-Error "Cannot find the Bicep CLI. Install Bicep CLI using Install-BicepCLI"
+        Write-Error "Cannot find the Bicep CLI. Install Bicep CLI using Install-BicepCLI."
     }
 }
 
+function ConvertFrom-Bicep {
+    param(
+        [string]$Path = $pwd.path
+    )
+
+    $bicep = (bicep --version)
+    if ($bicep) {
+        $files = Get-Childitem -Path $Path *.json -File
+        if ($files) {
+            foreach ($file in $files) {
+                bicep decompile $file
+            }   
+        }
+        else {
+            Write-Host "No bicep files located in path $Path"
+        } 
+    }
+    else {
+        Write-Error "Cannot find the Bicep CLI. Install Bicep CLI using Install-BicepCLI."
+    }    
+}
+
+
 function Get-BicepVersion {
-    $installedVersion = ((bicep --version) -split "\s+")[3]
+    $installedVersion = Get-InstalledBicepVersion
+    $latestVersion = Get-LatestBicepVersion
 
-    $latestVersion = Invoke-WebRequest -URI "https://api.github.com/repos/Azure/Bicep/releases/latest" | ConvertFrom-Json
-    $latestVersion = $latestVersion.tag_name -replace '[v]', ''
-
-    $version = @{
+    [pscustomobject]@{
         InstalledVersion = $installedVersion
         LatestVersion    = $latestVersion
-    }
-    $versions = New-Object psobject -Property $version
-    $versions 
+    } 
 }
 
 function Install-BicepCLI {
-    $bicep = (bicep --version)
-    if (-not $bicep) {
+    param(
+        [switch]$Force
+    )
+    if (-not $Force.IsPresent) {
+        try {
+            bicep --version
+            $BicepInstalled = $true
+        }
+        catch {
+            $BicepInstalled = $false
+        }
+    }
+    if ($Force.IsPresent -or $BicepInstalled -eq $false) {
         # Create the install folder
         $installPath = "$env:USERPROFILE\.bicep"
         $installDir = New-Item -ItemType Directory -Path $installPath -Force
@@ -52,32 +82,44 @@ function Install-BicepCLI {
         # Done!
     }
     else {
-        Write-Host "Bicep CLI is already installed. Use Update-BicepCLI to updated to the latest release"
+        $versionCheck = Compare-BicepVersion
+        if ($versionCheck) {
+            Write-Host "The latest Bicep CLI Version is already installed."
+        }
+        else {
+            Write-Host "Bicep CLI is already installed, but there is a newer release available. Use Update-BicepCLI or Install-BicepCLI -Force to updated to the latest release"   
+        }        
     }
 }
 
 function Update-BicepCLI {
-    $installedVersion = ((bicep --version) -split "\s+")[3]
+    $versionCheck = Compare-BicepVersion
 
-    $latestVersion = Invoke-WebRequest -URI "https://api.github.com/repos/Azure/Bicep/releases/latest" | convertfrom-json
-    $latestVersion = $latestVersion.tag_name -replace '[v]', ''
-
-    if ($installedVersion = $latestVersion) {
-        Write-Host "You are running the latest version of Bicep CLI"
+    if ($versionCheck) {
+        Write-Host "You are already running the latest version of Bicep CLI."
     }
     else {
-        # Create the install folder
-        $installPath = "$env:USERPROFILE\.bicep"
-        $installDir = New-Item -ItemType Directory -Path $installPath -Force
-        $installDir.Attributes += 'Hidden'
-        # Fetch the latest Bicep CLI binary
-        (New-Object Net.WebClient).DownloadFile("https://github.com/Azure/bicep/releases/latest/download/bicep-win-x64.exe", "$installPath\bicep.exe")
-        # Add bicep to your PATH
-        $currentPath = (Get-Item -path "HKCU:\Environment" ).GetValue('Path', '', 'DoNotExpandEnvironmentNames')
-        if (-not $currentPath.Contains("%USERPROFILE%\.bicep")) { setx PATH ($currentPath + ";%USERPROFILE%\.bicep") }
-        if (-not $env:path.Contains($installPath)) { $env:path += ";$installPath" }
-        # Verify you can now access the 'bicep' command.
-        bicep --help
-        # Done!
+        Install-BicepCLI -Force
     }     
+}
+
+function Get-LatestBicepVersion {
+    $latestVersion = Invoke-WebRequest -URI "https://api.github.com/repos/Azure/Bicep/releases/latest" | convertfrom-json
+    $latestVersion.tag_name -replace '[v]', ''
+}
+
+function Get-InstalledBicepVersion {
+    ((bicep --version) -split "\s+")[3]
+}
+
+function Compare-BicepVersion {
+    $installedVersion = Get-InstalledBicepVersion
+    $latestVersion = Get-LatestBicepVersion
+
+    if ($installedVersion = $latestVersion) {
+        $true
+    }
+    else {
+        $false
+    }
 }
