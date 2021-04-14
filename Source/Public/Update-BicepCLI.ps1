a6656b89857393ec96d9bfeb519a413d44e7de33
Mats Estensen <mats.estensen@devoteam.no>
2021-04-14 18:37:02 +0200

    fix: Error message on Windows-only commands
    - Install-BicepCLI
    - Update-BicepCLI
    - Uninstall-BicepCLI
    
    Closes #116

--- Source/Public/Update-BicepCLI.ps1
+++ Source/Public/Update-BicepCLI.ps1
@@ -2,14 +2,21 @@ function Update-BicepCLI {
     [CmdletBinding()]
     param (
     )
-    
+
+    if (!($IsWindows)) {
+        Write-Error -Message "This cmdlet is only supported for Windows systems. `
+To update Bicep on your system see instructions on https://github.com/Azure/bicep"
+        Write-Host "Compare your Bicep version with latest version by running Get-BicepVersion"
+        break
+    }
+
     $versionCheck = CompareBicepVersion
 
     if ($versionCheck) {
         Write-Host "You are already running the latest version of Bicep CLI."
     }
-    else {
+    elseif ($isWindows) {
         Uninstall-BicepCLI -Force -ErrorAction SilentlyContinue
         Install-BicepCLI -Force
-    }     
+    }
 }
