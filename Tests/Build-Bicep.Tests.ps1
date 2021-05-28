2761d54daf69001e5224554f3a0de2ccab026260
StefanIvemo <stefan@ivemo.se>
2021-05-28 14:09:23 +0200

    fixed formatting

--- Tests/Build-Bicep.Tests.ps1
+++ Tests/Build-Bicep.Tests.ps1
@@ -14,21 +14,17 @@ InModuleScope Bicep {
             BeforeAll {
                 $armTemaplate = Build-Bicep -Path .\supportFiles\workingBicep.bicep -AsString
             }
-
             It 'Build a bicep file' {
                 $armTemaplate | Should -Not -BeNullOrEmpty
             }
         }
-
         Context 'When it does not work' {
             BeforeAll {                
                 $armTemaplate = Build-Bicep -Path .\supportFiles\brokenBicep.bicep -AsString                
             }
-
             It 'Throws error if unable to get version file from GitHub' {
                 $armTemaplate | Should -BeNullOrEmpty
             }
-
         }
     }
 }
