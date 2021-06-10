b646778cf3a2220a707ff864147b1f482a0dd59e
StefanIvemo <stefan@ivemo.se>
2021-06-10 22:51:57 +0200

    updates according to feedback

--- Tests/Build-Bicep.Tests.ps1
+++ Tests/Build-Bicep.Tests.ps1
@@ -9,28 +9,28 @@ catch {
 InModuleScope Bicep {
     BeforeAll {
         $ScriptDirectory = Split-Path -Path $PSCommandPath -Parent
-        Copy-Item "$ScriptDirectory\supportFiles\*.bicep" -Destination TestDrive:\
+        Copy-Item "$ScriptDirectory\supportFiles\*" -Destination TestDrive:\
     }
     Describe 'Build-Bicep' {
         
         Context 'When it works' {            
             It 'Build a bicep file AsString' {
-                Build-Bicep -Path 'TestDrive:\workingBicep.bicep' -AsString | Should -Not -BeNullOrEmpty
+                Build-Bicep -Path 'TestDrive:\workingBicep.bicep' -AsString | Should -BeOfType System.String
             }
             It 'Build a bicep file AsHashtable' {
-                Build-Bicep -Path 'TestDrive:\workingBicep.bicep' -AsHashtable | Should -Not -BeNullOrEmpty
+                Build-Bicep -Path 'TestDrive:\workingBicep.bicep' -AsHashtable | Should -BeOfType System.Collections.Specialized.OrderedDictionary
             }
             It 'Build a bicep file' {
                 $file='TestDrive:\workingBicep.bicep'
                 Build-Bicep -Path $file
                 $templateFile = $file -replace '\.bicep', '.json'
-                Get-Content -Path $templateFile -Raw | Should -Not -BeNullOrEmpty
+                Get-Content -Path $templateFile -Raw | ConvertFrom-Json | Should -Not -BeNullOrEmpty
             }
             It 'Build a bicep file and generate parameters' {
                 $file='TestDrive:\workingBicep.bicep'
                 Build-Bicep -Path $file -GenerateAllParametersFile
                 $parameterFile = $file -replace '\.bicep', '.parameters.json'
-                Get-Content -Path $parameterFile -Raw | Should -Not -BeNullOrEmpty
+                Get-Content -Path $parameterFile -Raw | ConvertFrom-Json | Should -Not -BeNullOrEmpty
             }
 
         }
