0b8ffa24421fcc6bc7fa0bb09d3f83444f4d1af8
StefanIvemo <stefan@ivemo.se>
2021-06-27 23:19:30 +0200

    Fixed issue with deleted params

--- Source/Public/Update-BicepParameterFile.ps1
+++ Source/Public/Update-BicepParameterFile.ps1
@@ -34,9 +34,10 @@ function Update-BicepParameterFile {
         
         $FileName = $ParamFile.BaseName.Replace('.parameters', '')
         
+        # Try to find a matching Bicep template for the provided parameter file based on its file name
         if (-not $PSBoundParameters.ContainsKey('BicepFile')) {
             $BicepFilePath = (Get-ChildItem $ParamFile.DirectoryName -Filter *.bicep | Where-Object { $_.BaseName -eq $FileName }).FullName
-
+            
             if (-not $BicepFilePath) {
                 Write-Error "Cant find BicepFile Named $FileName in directory: $($ParamFile.DirectoryName)"
                 Break
@@ -49,12 +50,44 @@ function Update-BicepParameterFile {
             }
             $BicepFilePath = $BicepFile
         }
+        
+        # Import the old paramter file and convert it to an ordered hashtable
+        $oldParametersFile = Get-Content -Path $Path | ConvertFrom-Json -Depth 100 | ConvertToHashtable -Ordered
 
+        # Generate a temporary Bicep Parameter File with all paramters
         $BicepFileName = (Get-Item -Path $BicepFilePath).BaseName
-        New-BicepParameterFile -Path $BicepFilePath -OutputDirectory $tempPath -Parameters $Parameters
+        New-BicepParameterFile -Path $BicepFilePath -OutputDirectory $tempPath -Parameters All
 
-        $NewParametersFilePath = $tempPath+"$BicepFileName.parameters.json"
+        $allParametersFilePath = $tempPath + "$($BicepFileName).parameters.json"
 
+        # Convert the all parameters file to an ordered hashtable
+        try {
+            $allParametersFile = Get-Content -Path $allParametersFilePath -ErrorAction Stop | ConvertFrom-Json -Depth 100 | ConvertToHashtable -Ordered
+        }
+        catch {
+            Write-Error "Failed to create Bicep ParameterObject."
+            Break
+        }        
+
+        # Remove any deleted parameters from old parameter file
+        $oldParameterArray = @()
+        $oldParametersFile.parameters.Keys.ForEach( { $oldParameterArray += $PSItem })
+        
+        foreach ($item in $oldParameterArray) {
+            if (!$allParametersFile.parameters.Contains($item)) {
+                $oldParametersFile.parameters.Remove($item)
+            }
+        }
+        
+        
+        # Generate a new temporary Bicep Parameter File if -Parameters is set to Required
+        if ($Parameters -eq 'Required') {
+            $BicepFileName = (Get-Item -Path $BicepFilePath).BaseName
+            New-BicepParameterFile -Path $BicepFilePath -OutputDirectory $tempPath -Parameters $Parameters
+        }
+        $NewParametersFilePath = $tempPath + "$BicepFileName.parameters.json"
+        
+        # Convert the new paramter file to an ordered hashtable
         try {
             $NewParametersFile = Get-Content -Path $NewParametersFilePath -ErrorAction Stop | ConvertFrom-Json -Depth 100 | ConvertToHashtable -Ordered
         }
@@ -63,17 +96,19 @@ function Update-BicepParameterFile {
             Break
         }
         
-        $OldParametersFile = Get-Content -Path $Path | ConvertFrom-Json -Depth 100 | ConvertToHashtable -Ordered
-
+        # Create an array with the new parameters
         $ParameterArray = @()
-        $NewParametersFile.parameters.Keys.ForEach({ $ParameterArray += $PSItem })
+        $NewParametersFile.parameters.Keys.ForEach( { $ParameterArray += $PSItem })
         
+        # Iterate over the new paramters and add any missing to the old parameters array
         foreach ($item in $ParameterArray) {
-            if ($OldParametersFile.parameters.Contains($item)) {
-                $NewParametersFile.parameters[$item] = $OldParametersFile.parameters[$item]
+            if (!$oldParametersFile.parameters.Contains($item)) {
+                $oldParametersFile.parameters[$item] = @{                                
+                    value = $NewParametersFile.parameters.$item.value
+                }
             }
         }
-        $NewParametersFile | ConvertTo-Json -Depth 100 | Out-File -Path $Path -Force
+        $oldParametersFile | ConvertTo-Json -Depth 100 | Out-File -Path $Path -Force
         
     }
     end {
