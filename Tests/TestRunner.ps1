206eb49d8131b6eeec2e321f2f5b2f289ac8fa39
John Roos <john.m.roos@gmail.com>
2021-06-30 21:56:33 +0200

    TestRunner now generates code coverage

--- Tests/TestRunner.ps1
+++ Tests/TestRunner.ps1
@@ -1,7 +1,14 @@
 Import-Module -Name 'Pester'
 
+$functions = [System.Collections.ArrayList]::new()
+$functions.AddRange((Get-ChildItem "$PSScriptRoot\..\Source\Classes\*.ps1").FullName)
+$functions.AddRange((Get-ChildItem "$PSScriptRoot\..\Source\Private\*.ps1").FullName)
+$functions.AddRange((Get-ChildItem "$PSScriptRoot\..\Source\Public\*.ps1").FullName)
+
 $PesterConfiguration = [PesterConfiguration]::new()
 $PesterConfiguration.TestResult.Enabled = $true
 $PesterConfiguration.TestResult.OutputFormat = 'JUnitXml'
+$PesterConfiguration.CodeCoverage.Enabled = $true
+$PesterConfiguration.CodeCoverage.Path = $functions.ToArray()
 
 Invoke-Pester -Configuration $PesterConfiguration
