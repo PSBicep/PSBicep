4499dd4e8c61cbe8c5b27438abfa4582c43e19f5
John Roos <john.m.roos@gmail.com>
2021-06-30 22:04:51 +0200

    Only use InModuleScope where needed

--- Tests/InstalledBicepVersion.Tests.ps1
+++ Tests/InstalledBicepVersion.Tests.ps1
@@ -5,34 +5,35 @@ BeforeAll {
     Import-Module -FullyQualifiedName "$ScriptDirectory\..\Source\Bicep.psd1" -ErrorAction Stop
 }
 
-InModuleScope Bicep { 
-    Describe 'InstalledBicepVersion' {
-        Context 'Bicep CLI installed' {
-            BeforeAll {
+Describe 'InstalledBicepVersion' {
+    Context 'Bicep CLI installed' {
+        BeforeAll {
+            Mock TestBicep -ModuleName Bicep {
+                $true
+            }
+        }
+
+        It 'Returns correctly parsed version' {
+            InModuleScope Bicep {
                 # We cannot properly mock regular commands like bicep.exe
                 # Instead, we use a function which kind of works like a mock, with very limited functionality
                 function bicep {
                     'Bicep CLI version 0.12.987 (a13b032755)'
                 }
-
-                Mock TestBicep -ModuleName Bicep {
-                    $true
-                }
-            }
-
-            It 'Returns correctly parsed version' {
                 InstalledBicepVersion | Should -Be '0.12.987'
             }
         }
+    }
 
-        Context 'Bicep CLI not installed' {
-            BeforeAll {
-                Mock TestBicep -ModuleName Bicep {
-                    $false
-                }
+    Context 'Bicep CLI not installed' {
+        BeforeAll {
+            Mock TestBicep -ModuleName Bicep {
+                $false
             }
+        }
 
-            It 'Returns not installed message' {
+        It 'Returns not installed message' {
+            InModuleScope Bicep {
                 InstalledBicepVersion | Should -Be 'Not installed'
             }
         }
