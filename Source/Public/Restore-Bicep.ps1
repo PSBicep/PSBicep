3de825d73392575e898666d2ef5b2c6c211c99a1
Stefan Ivemo <stefan@ivemo.se>
2021-12-06 09:04:06 +0100

    Added Restore-Bicep

---
+++ Source/Public/Restore-Bicep.ps1
@@ -0,0 +1,27 @@
+function Restore-Bicep {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, Position = 1)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Path         
+    )
+
+    # Check if a newer version of the module is published
+    if (-not $Script:ModuleVersionChecked) {
+        TestModuleVersion
+    }
+
+    # Verbose output Bicep Version used
+    $FullVersion = Get-BicepNetVersion -Verbose:$false
+    Write-Verbose -Message "Using Bicep version: $FullVersion"
+  
+    # Restore modules
+    try {
+        Restore-BicepNetFile -Path $Path
+        Write-Verbose -Message "Successfully restored all modules"
+    }
+    catch {
+        Throw $_
+    }
+
+}
