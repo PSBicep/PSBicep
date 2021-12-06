325fb844b2a2797b24be6218785c40e8b8247d05
Stefan Ivemo <stefan@ivemo.se>
2021-12-06 21:55:26 +0100

    fixed file not found error when using testdrive

--- Source/Public/Publish-Bicep.ps1
+++ Source/Public/Publish-Bicep.ps1
@@ -9,27 +9,29 @@ function Publish-Bicep {
         [ValidateNotNullOrEmpty()]
         [string]$Target         
     )
+    begin {
+        # Check if a newer version of the module is published
+        if (-not $Script:ModuleVersionChecked) {
+            TestModuleVersion
+        }
 
-    # Check if a newer version of the module is published
-    if (-not $Script:ModuleVersionChecked) {
-        TestModuleVersion
+        # Verbose output Bicep Version used
+        $FullVersion = Get-BicepNetVersion -Verbose:$false
+        Write-Verbose -Message "Using Bicep version: $FullVersion"
     }
 
-    # Verbose output Bicep Version used
-    $FullVersion = Get-BicepNetVersion -Verbose:$false
-    Write-Verbose -Message "Using Bicep version: $FullVersion"
-
-    $BicepFile= Get-Childitem -Path $Path *.bicep -File
-    $LoginServer=(($target -split ":")[1] -split "/")[0]
+    process {
+        $BicepFile = Get-Childitem -Path $Path *.bicep -File
+        $LoginServer = (($target -split ":")[1] -split "/")[0]
     
-    # Publish module
-    try {
-        Publish-BicepNetFile -Path $Path -Target $Target
-        Write-Verbose -Message "Successfully authenticated to $LoginServer"
-        Write-Verbose -Message "[$($BicepFile.Name)] published to: [$Target]"
+        # Publish module
+        try {
+            Publish-BicepNetFile -Path $BicepFile.FullName -Target $Target
+            Write-Verbose -Message "Successfully authenticated to $LoginServer"
+            Write-Verbose -Message "[$($BicepFile.Name)] published to: [$Target]"
+        }
+        catch {
+            Throw $_
+        }
     }
-    catch {
-        Throw $_
-    }
-
 }
