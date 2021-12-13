365dc33bdb3c62c867e294934343efa40552e4cc
John Roos <john.m.roos@gmail.com>
2021-12-13 21:47:33 +0100

    Added tests to ListBicepVersions

--- Tests/ListBicepVersions.Tests.ps1
+++ Tests/ListBicepVersions.Tests.ps1
@@ -4,40 +4,55 @@ BeforeAll {
     Import-Module -FullyQualifiedName "$ScriptDirectory\..\Source\Bicep.psd1" -ErrorAction Stop
 }
 
+Describe 'ListBicepVersions' {
 
-
-    Describe 'ListBicepVersions' {
+    Context 'When it works' {
         
-        Context 'When it works' {
-            
-            BeforeAll {
-                Mock Invoke-RestMethod -ModuleName Bicep {
-                    [PSCustomObject]@{
-                        tag_name = 'v1.2.3'
-                    }
-                }
-            }
-
-            It 'Returns correct version' {
-                InModuleScope Bicep {
-                    ListBicepVersions -Latest | Should -Be '1.2.3'
+        BeforeAll {
+            Mock Invoke-RestMethod -ModuleName Bicep {
+                if ($Uri -like '*latest') {
+                    @{ tag_name = 'v1.2.3' }
+                } else {
+                    @{ tag_name = 'v1.2.3' }
+                    @{ tag_name = 'v1.2.2' }
+                    @{ tag_name = 'v1.2.1' }
                 }
             }
         }
 
-        Context 'When it does not work' {
-           
-            BeforeAll {
-                Mock Invoke-RestMethod -ModuleName Bicep {
-                    Throw 'Not working'
-                }
+        It 'Returns correct latest version' {
+            InModuleScope Bicep {
+                ListBicepVersions -Latest | Should -Be '1.2.3'
             }
+        }
 
-            It 'Throws error if unable to get version file from GitHub' {
-                InModuleScope Bicep {
-                    { ListBicepVersions -Latest -ErrorAction Stop } | Should -Throw "Could not get latest version from GitHub.*"
-                }
+        It 'Returns all versions' {
+            InModuleScope Bicep {
+                $versions = ListBicepVersions
+                $versions.count | Should -Be 3
             }
-
         }
     }
+
+    Context 'When it does not work' {
+        
+        BeforeAll {
+            Mock Invoke-RestMethod -ModuleName Bicep {
+                Throw 'Not working'
+            }
+        }
+
+        It 'Throws error if unable to get latest version file from GitHub' {
+            InModuleScope Bicep {
+                { ListBicepVersions -Latest -ErrorAction Stop } | Should -Throw "Could not get latest version from GitHub.*"
+            }
+        }
+
+        It 'Throws error if unable to get all version files from GitHub' {
+            InModuleScope Bicep {
+                { ListBicepVersions -ErrorAction Stop } | Should -Throw "Could not get versions from GitHub.*"
+            }
+        }
+
+    }
+}
