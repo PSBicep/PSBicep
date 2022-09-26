e8e988da7965b74ee2ad400ac6fab222c4e4fda7
Stefan Ivemo <stefan@ivemo.se>
2022-09-26 21:59:06 +0200

    Added new command Get-BicepMetadata

---
+++ Source/Public/Get-BicepMetadata.ps1
@@ -0,0 +1,57 @@
+function Get-BicepMetadata {
+    [CmdletBinding()]
+    param (
+        # Specifies a path to one or more locations.
+        [Parameter(
+            Mandatory = $true
+        )]
+        [Alias("PSPath")]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Path,
+
+        # Set output type
+        [Parameter()]
+        [ValidateSet('Simple', 'Json', 'Hashtable')]
+        [String]
+        $OutputType = 'Simple'
+    )
+    
+    begin {
+        if (-not $Script:ModuleVersionChecked) {
+            TestModuleVersion
+        }
+
+        if ($VerbosePreference -eq [System.Management.Automation.ActionPreference]::Continue) {
+            $FullVersion = Get-BicepNetVersion -Verbose:$false
+            Write-Verbose -Message "Using Bicep version: $FullVersion"
+        }
+    }
+    
+    process {
+        $file = Get-Item -Path $Path
+        try {  
+       
+            $BuildResult = Build-BicepNetFile -Path $file.FullName
+
+            $ARMTemplate = $BuildResult[0]
+            $ARMTemplateObject = ConvertFrom-Json -InputObject $ARMTemplate
+           
+        }
+        catch {
+            # We don't care about errors here.
+        }        
+
+        switch ($OutputType) {
+            'Simple' {
+                $ARMTemplateObject.metadata
+            }
+            'Json' {
+                $ARMTemplateObject.metadata | ConvertTo-Json
+            }
+            'Hashtable' {
+                $ARMTemplateObject.metadata | ConvertToHashtable -Ordered
+            }
+        }
+    }
+}
