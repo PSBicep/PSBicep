58484940ad02632e75ab47c7eea35f84f50d421d
Simon Wåhlin <simon.wahlin@advania.com>
2023-09-11 15:29:55 +0200

    Update actions

--- .build/tasks/PSBicep.GetBicepTypes.ps1
+++ .build/tasks/PSBicep.GetBicepTypes.ps1
@@ -1,5 +1,8 @@
 task getBicepTypes {
-    $AssetsFolder = Resolve-Path -Path 'Source/Assets'
+    $AssetsFolderPath = 'Source/Assets'
+    if(-not (Test-Path -Path $AssetsFolderPath)) {
+        $null = New-Item -Path $AssetsFolderPath -ItemType Directory
+    }
 
     # Download Bicep types
     $BicepTypesFull = Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/Azure/bicep-types-az/main/generated/index.json'
@@ -9,6 +12,6 @@ task getBicepTypes {
 
     # Filter out the resources and save to disk
     $BicepTypesFiltered = ConvertTo-Json -InputObject $BicepTypesFull.Resources.psobject.Properties.name -Compress
-    $BicepTypesPath = Join-Path -Path $AssetsFolder.Path -ChildPath 'BicepTypes.json'
+    $BicepTypesPath = Join-Path -Path $AssetsFolderPath -ChildPath 'BicepTypes.json'
     Out-File -FilePath $BicepTypesPath -InputObject $BicepTypesFiltered
 }
