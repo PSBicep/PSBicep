38dacdfca57f5e847d469a015f5d89af878a6417
Simon Wåhlin <simon@simonw.se>
2023-09-19 07:59:19 +0200

    Fix build action (#320)
    * Fix build action

--- .build/tasks/PSBicep.CopyBicepNetPS.ps1
+++ .build/tasks/PSBicep.CopyBicepNetPS.ps1
@@ -1,11 +1,16 @@
-task copyBicepNetPS {
+task copyBicepNetPSToOutput {
     $RequiredBicepNetModuleVersion = Get-Metadata -Path 'RequiredModules.psd1' -PropertyName 'BicepNet.PS' -ErrorAction 'Stop'
-    $ModuleVersion = Get-BuiltModuleVersion -OutputDirectory 'output' -ModuleName 'Bicep' -VersionedOutputDirectory
-    $ModuleOutputPath = "output/Bicep/$ModuleVersion"
+    $ModuleVersion = Split-ModuleVersion -ModuleVersion (Get-BuiltModuleVersion -OutputDirectory 'output' -ModuleName 'Bicep' -VersionedOutputDirectory)
+    $ModuleOutputPath = "output/Bicep/$($ModuleVersion.Version)"
+    $null = New-Item -Path "$ModuleOutputPath/BicepNet.PS" -ItemType 'Directory' -Force -ErrorAction 'Stop'
+    Copy-Item -Path "output/RequiredModules/BicepNet.PS/$RequiredBicepNetModuleVersion" -Destination "$ModuleOutputPath/BicepNet.PS/" -Force -Recurse -ErrorAction 'Stop'
+    Update-Metadata -Path "$ModuleOutputPath/Bicep.psd1" -PropertyName 'NestedModules' -Value @("./BicepNet.PS/$RequiredBicepNetModuleVersion/BicepNet.PS.psd1") -ErrorAction 'Stop'
+}
+
+task copyBicepNetPSToSource {
+    $RequiredBicepNetModuleVersion = Get-Metadata -Path 'RequiredModules.psd1' -PropertyName 'BicepNet.PS' -ErrorAction 'Stop'
     $ModuleSourcePath = "Source/"
-    foreach($Path in $ModuleOutputPath, $ModuleSourcePath) {
-        $null = New-Item -Path "$Path/BicepNet.PS" -ItemType 'Directory' -Force -ErrorAction 'Stop'
-        Copy-Item -Path "output/RequiredModules/BicepNet.PS/$RequiredBicepNetModuleVersion" -Destination "$Path/BicepNet.PS/" -Force -Recurse -ErrorAction 'Stop'
-        Update-Metadata -Path "$Path/Bicep.psd1" -PropertyName 'NestedModules' -Value @("./BicepNet.PS/$RequiredBicepNetModuleVersion/BicepNet.PS.psd1") -ErrorAction 'Stop'
-    }
+    $null = New-Item -Path "$ModuleSourcePath/BicepNet.PS" -ItemType 'Directory' -Force -ErrorAction 'Stop'
+    Copy-Item -Path "output/RequiredModules/BicepNet.PS/$RequiredBicepNetModuleVersion" -Destination "$ModuleSourcePath/BicepNet.PS/" -Force -Recurse -ErrorAction 'Stop'
+    Update-Metadata -Path "$ModuleSourcePath/Bicep.psd1" -PropertyName 'NestedModules' -Value @("./BicepNet.PS/$RequiredBicepNetModuleVersion/BicepNet.PS.psd1") -ErrorAction 'Stop'
 }
--- Source/Bicep.psd1
+++ Source/Bicep.psd1
@@ -152,7 +152,7 @@ PrivateData = @{
         ReleaseNotes = 'https://github.com/PSBicep/PSBicep/releases'
 
         # Prerelease string of this module
-        # Prerelease = ''
+        Prerelease = ''
 
         # Flag to indicate whether the module requires explicit user acceptance for install/update/save
         # RequireLicenseAcceptance = $false
