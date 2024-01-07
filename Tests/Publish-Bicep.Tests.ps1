21f75418e3a2f79142d10b344bb19ca4a83fdd89
Emanuel Palm <emanuel+github@pipe.how>
2024-01-07 16:25:23 +0100

    publish tests assembly conflict

--- Tests/Publish-Bicep.Tests.ps1
+++ Tests/Publish-Bicep.Tests.ps1
@@ -1,5 +1,4 @@
 BeforeAll {
-    Import-Module -FullyQualifiedName "$PSScriptRoot\..\Source\BicepNet.PS" -ErrorAction Stop
     Import-Module -FullyQualifiedName "$PSScriptRoot\..\Source\Bicep.psd1" -ErrorAction Stop
     Copy-Item "$PSScriptRoot\supportFiles\*" -Destination TestDrive:\
 }
