de95cb2bde9f083f99f37d84419bfd6e39c95ea5
Simon Wåhlin <simon@simonw.se>
2024-10-04 16:42:02 +0200

    "Fix" task casing by hardcoding path

--- .build/tasks/PlatyPS.RepairPlatyPSHelp.ps1
+++ .build/tasks/PlatyPS.RepairPlatyPSHelp.ps1
@@ -127,5 +127,5 @@ function Repair-PlatyPSMarkdown {
 
 task repairPlatyPSHelp {
     # Workaround to run post-build to avoid platyPS generating documentation for common parameter ProgressAction
-    Repair-PlatyPSMarkdown -Path (Get-ChildItem "$BuildRoot/$HelpSourceFolder/$HelpOutputFolder") -ParameterName 'ProgressAction'
+    Repair-PlatyPSMarkdown -Path (Get-ChildItem "$BuildRoot/Docs/Help") -ParameterName 'ProgressAction'
 }
