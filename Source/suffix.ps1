1e7c2ce77f05ac93fd5a361c316556a5ebad45b0
Simon Wåhlin <simon.wahlin@unwonted.se>
2024-12-27 10:01:39 +0100

    Improve ModuleVersion Test

--- Source/suffix.ps1
+++ Source/suffix.ps1
@@ -1,7 +1,6 @@
-$BicepTypesPath = Join-Path -Path $PSScriptRoot -ChildPath 'Assets/BicepTypes.json'
+$script:BicepTypesPath = Join-Path -Path $PSScriptRoot -ChildPath 'Assets/BicepTypes.json'
+$script:ModuleManifestPath = Join-Path -Path $PSScriptRoot -ChildPath 'Bicep.psd1'
 $script:TokenSplat = @{}
 Write-Verbose "Preloading Bicep types from: '$BicepTypesPath'"
 $null = GetBicepTypes -Path "$BicepTypesPath"
-if (-not $Script:ModuleVersionChecked) {
-    TestModuleVersion
-}
+TestModuleVersion
