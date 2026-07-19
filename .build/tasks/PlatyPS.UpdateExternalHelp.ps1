8711b53d6e807bc22c082bf7713690eb7e607e35
Simon Wåhlin <simon.wahlin@unwonted.se>
2026-07-19 09:53:05 +0200

    Update build chain for new samlpler version

--- .build/tasks/PlatyPS.UpdateExternalHelp.ps1
+++ .build/tasks/PlatyPS.UpdateExternalHelp.ps1
@@ -1,14 +1,14 @@
 task updateExternalHelp {
     $ProjectName = Get-SamplerProjectName -BuildRoot $BuildRoot
+    $ModuleInfo = Import-Module -Name $ProjectName -PassThru -Force -ErrorAction 'Stop'
 
-    $OutputDocsDestination = (Get-Item ./output/$ProjectName/**/en-US | Select-Object -Last 1).FullName
+    $OutputDocsDestination = Join-Path -Path $ModuleInfo.ModuleBase -ChildPath 'en-US'
     Import-Module 'Microsoft.PowerShell.PlatyPS' -ErrorAction 'Stop'
     Measure-PlatyPSMarkdown -Path ./Docs/$ProjectName/*.md |
         Where-Object Filetype -match 'CommandHelp' |
         Import-MarkdownCommandHelp -Path {$_.FilePath} |
         Export-MamlCommandHelp -OutputFolder $OutputDocsDestination -Force
 
-    $ModuleInfo = Import-Module -Name $ProjectName -PassThru -ErrorAction 'Stop'
     $newMarkdownCommandHelpSplat = @{
         ModuleInfo = $ModuleInfo
         OutputFolder = "./Docs"
